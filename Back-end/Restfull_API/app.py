ff990f73ce3fa09968be9413157e0cf47e0887e6
DESKTOP-GO7LKQN\ADMIN <anhnguyenfrance103@gmail.com>
2024-06-24 21:22:20 +0700

    first commit

---
+++ Back-end/Restfull_API/app.py
@@ -0,0 +1,5 @@
+from library import create_app
+
+if __name__ == "__name__":
+    app = create_app()
+    app.run(debug=True)
---
+++ Back-end/Restfull_API/library/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask, request , Blueprint
+
+def create_app(config_file = "config.py"):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    print(app.config("SECRET_KEY"))
+    return app
