5e3297d9313e2cb6aac38a5c1721dae38074b902
DESKTOP-GO7LKQN\ADMIN <anhnguyenfrance103@gmail.com>
2024-06-29 21:50:15 +0700

    xoa Back-End cu, cap nhat them VueX ,chinh sua giao dien SubNav

--- Back-end/manage.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
